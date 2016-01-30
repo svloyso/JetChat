1f52d735808fc356b68de9062197bd112f26b343
cheptsov <andrey.cheptsov@jetbrains.com>
2016-01-30 14:43:21 +0100

    Support read/unread messages #1

--- public/javascripts/components/topic-item.js
+++ public/javascripts/components/topic-item.js
@@ -1,16 +1,21 @@
 import React from 'react';
-var prettydate = require("pretty-date");
+import PrettyDate from 'pretty-date';
 import ChatActions from '../events/chat-actions';
+import VisibilitySensor from 'react-visibility-sensor';
 
 var TopicItem = React.createClass({
     onClick: function (topic) {
         ChatActions.selectTopic(topic);
     },
 
+    onChange: function (isVisible) {
+        console.log('Topic %s is now %s', this.props.topic.text, isVisible ? 'visible' : 'hidden');
+    },
+
     render: function () {
         var self = this;
         var topicClass = self.props.selected ? "selected" : "";
-        var prettyDate = prettydate.format(new Date(self.props.updateDate));
+        var prettyDate = PrettyDate.format(new Date(self.props.updateDate));
         var groupRef, unreadLabel;
         if (self.props.showGroup) {
             // TODO: Cannot read property 'name' of undefined
@@ -23,6 +28,7 @@ var TopicItem = React.createClass({
         return (
             <li data-topic={self.props.topic.id} className={topicClass}
                 onClick={self.onClick.bind(self, self.props.topic)}>
+                <VisibilitySensor onChange={self.onChange.bind(self)} />
                 <div className="text">{self.props.topic.text}</div>
                 <div className="info">
                     <span className="author">{self.props.topic.user.name}</span>
