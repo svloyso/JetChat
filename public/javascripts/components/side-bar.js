478cf51b907c9b67d09d7f101e757b127325484d
cheptsov <andrey.cheptsov@jetbrains.com>
2016-02-13 22:52:20 +0100

    Experiment with Travis and Electron Settings icon with logout link

--- public/javascripts/components/side-bar.js
+++ public/javascripts/components/side-bar.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import GroupPane from './group-pane';
+import {Dropdown, MenuItem} from 'react-bootstrap';
 
 var SideBar = React.createClass({
+    onLogout: function(e) {
+        window.location.replace("/logout")
+    },
+
     render: function () {
         return (
             <div id="side-bar">
-                <div id="header">JetChat</div>
+                <div id="header"><span>JetChat</span>
+                    <Dropdown id="settings-dropdown">
+                        <a href="#" bsRole="toggle">
+                            <span id="gear"></span>
+                        </a>
+                        <Dropdown.Menu bsRole="menu">
+                            <MenuItem eventKey="2" onSelect={this.onLogout}>Logout</MenuItem>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
                 <GroupPane/>
             </div>
         );
