36c56590e8a848e096e6ecc625863a7455976b3d
cheptsov <andrey.cheptsov@jetbrains.com>
2016-03-04 09:11:41 +0100

    Performance is unacceptably poor #44 (bugfix)

--- app/models/User.scala
+++ app/models/User.scala
@@ -64,8 +64,8 @@ class UsersDAO @Inject()(val dbConfigProvider: DatabaseConfigProvider)
                 LEFT JOIN users u ON u.id = d.to_user_id OR u.id = d.from_user_id
                 LEFT JOIN direct_message_read_statuses ds ON ds.direct_message_id = d.id
                 LEFT JOIN last_direct_messages ld ON ld.min_user_id = LEAST($userId, u.id) AND ld.max_user_id = GREATEST($userId, u.id)
-              WHERE (d.from_user_id = $userId OR d.to_user_id = 9) AND u.id <> $userId
-              GROUP BY u.id, u.login, u.name, u.avatar
+              WHERE (d.from_user_id = $userId OR d.to_user_id = $userId) AND u.id <> $userId
+              GROUP BY u.id, u.login, u.name, u.avatar, ld.date, ld.text
               ORDER BY date DESC""".as[(Long, String, String, Option[String], Timestamp, String, Int, Int)]
       ).map { case results =>
         val myMessages = results.map { case (id, login, name, avatar, updateDate, text, readCount, totalCount) =>
