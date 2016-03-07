effc46b5e0f9638519efe053253e53a93b0247b4
Kirill Shmakov <shmakir@yandex.ru>
2016-03-07 12:58:36 +0300

    Search-pane cleaner visible only when it makes sense.

--- public/javascripts/components/search-pane.js
+++ public/javascripts/components/search-pane.js
@@ -6,19 +6,18 @@ import ChatStore from '../events/chat-store';
 var SearchPane = React.createClass({
     mixins: [Reflux.connect(ChatStore, 'store')],
 
-    setQuery: function() {
+    onChange: function () {
         this.state.store.query = ReactDOM.findDOMNode(this.refs.searchQuery).value.trim();
-    },
-
-    onKeyPress: function (event) {
-        if (event.which == 13) {
-            this.setQuery();
+        if (this.state.store.query !== "") {
+            ReactDOM.findDOMNode(this.refs.searchQueryCleaner).style.visibility="visible";
+            alert(this.state.store.query);
         }
     },
 
     onClearSearch: function() {
         this.state.store.query = "";
         ReactDOM.findDOMNode(this.refs.searchQuery).value = "";
+        ReactDOM.findDOMNode(this.refs.searchQueryCleaner).style.visibility="hidden";
     },
 
     render: function () {
@@ -27,8 +26,8 @@ var SearchPane = React.createClass({
                 <div className="btn-group">
                     <input ref="searchQuery" id="search" type="text" className="search-query"
                            placeholder="Search people, groups, topics, and messages"
-                           autoComplete="off" onKeyPress={this.onKeyPress}/>
-                    <span id="clear-search" onClick={this.onClearSearch}></span>
+                           autoComplete="off" onChange={this.onChange}/>
+                    <span ref="searchQueryCleaner" id="clear-search" onClick={this.onClearSearch}></span>
                 </div>
             </div>
         );
